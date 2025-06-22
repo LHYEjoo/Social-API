b6099d20016965d39ed7455a52377d4a8670369a
LHYE_joo <linhuiye@outlook.com>
2025-06-22 02:33:42 +0200

    feed now showing multiple stories

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const posts = require('./data/posts');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
